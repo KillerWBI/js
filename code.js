2927ac356675408ce1e77574db2b5c35d9fa56fe
KillerWBI <108660418+KillerWBI@users.noreply.github.com>
2022-08-12 20:29:01 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,30 +1,46 @@
-let m = prompt("ведите массу тела в килограммах:");
-console.log(`маса ${m} `);
+//let m = prompt("ведите массу тела в килограммах:");
+//console.log(`маса ${m} `);
 
-let h = prompt("ведите рост в метрах: Например(1.70)");
-console.log(`маса ${h} `); 
+//let h = prompt("ведите рост в метрах: Например(1.70)");
+//console.log(`маса ${h} `); 
 
-let sum = m / (h * h);
-sum = sum * 100;
-sum = Math.round(sum);
-sum = sum / 100;
-console.log(`Индекс масы тела ${sum} кг/м2`);
+//let sum = m / (h * h);
+//sum = sum * 100;
+//sum = Math.round(sum);
+//sum = sum / 100;
+//console.log(`Индекс масы тела ${sum} кг/м2`);
 
 
-if (sum<16){
-     console.log("Выраженный дефицит массы тела")
-}else if(sum<18.5){
-    console.log("Недостаточная (дефицит) масса тела")
-}else if(sum<25){
-    console.log("Норма")
-}else if(sum<30){
-    console.log("Избыточная масса тела (предожирение)")
-}else if(sum<35){
-    console.log("Ожирение 1 степени")
-}else if(sum<40){
-    console.log("Ожирение 2 степени")
-}else if(sum>40){
-    console.log("Ожирение 3 степени")
+//if (sum<16){
+//     console.log("Выраженный дефицит массы тела")
+//}else if(sum<18.5){
+//   console.log("Недостаточная (дефицит) масса тела")
+//}else if(sum<25){
+//    console.log("Норма")
+//}else if(sum<30){
+//    console.log("Избыточная масса тела (предожирение)")
+//}else if(sum<35){
+//    console.log("Ожирение 1 степени")
+//}else if(sum<40){
+//   console.log("Ожирение 2 степени")
+//}else if(sum){
+//    console.log("Ожирение 3 степени")
+//}
+
+
+
+
+let year = prompt("ведите год:");
+
+let wes = year / 4;
+
+wes = wes - wes ;
+
+console.log(`${wes} это высокосный год`)
+
+if (wes = 0){
+    wes = wes
+   console.log(`${year} это высокосный год`)
+}else{
+   console.log(`${year} это не высокосный год`)
 }
-
-
